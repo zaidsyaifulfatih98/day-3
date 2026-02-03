1f6aa201f9b681b0e8b22a6d1d7f8d2915c33219
zaidsyaifulfatih98 <86652453+zaidsyaifulfatih98@users.noreply.github.com>
2026-02-03 09:49:10 +0700

    name,age,status

---
+++ exercise_3.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")({sigint:true}) 
+
+const UserName = prompt ("masukkan nama anda :")
+console.log(UserName)
+
+const age = prompt("masukkan umur anda : ")
+console.log(age)
+
+const condition = prompt("masukkan status anda: ")
+console.log(condition)
